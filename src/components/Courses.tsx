0bab9449618f5fe13cc53d677f024b55f835176b
Laura Gao <gaolauro@gmail.com>
2023-09-17 10:43:44 -0400

    setup tailwind

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,13 +1,18 @@
-import courses from '../data/courses.json'
+import courses from '../data/courses.json';
 
 const Courses = () => (
   <>
-    {courses.map(({ dept, number}) => (
-      <p key={`${dept}-${number}`}>
-        {dept}
-        {' '}
-        {number}
-      </p>
+    {courses.map(({ dept, number, description }) => (
+      <div key={`${dept}-${number}`} className="mb-8">
+        <h2 className="font-bold text-3xl">
+          {dept}
+          {' '}
+          {number}
+        </h2>
+        <p>
+          {description}
+        </p>
+      </div>
     ))}
   </>
 )
